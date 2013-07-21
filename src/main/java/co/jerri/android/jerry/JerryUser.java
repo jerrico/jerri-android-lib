ae44665d05660a4c3ba70288c9ea3217b9c78a70
Benjamin Kampmann <ben.kampmann@gmail.com>
2013-07-21 17:43:10 +0200

    first working test

--- src/main/java/co/jerri/android/jerry/JerryUser.java
+++ src/main/java/co/jerri/android/jerry/JerryUser.java
@@ -29,4 +29,26 @@ public class JerryUser {
 	public boolean default_reaction;
 	private HashMap<String, List<Restriction>> restrictions;
 	private String profile_state;
+
+	public void load_state(String input){
+
+	}
+
+	public JerryUser(String n_user_id, String n_device_id, String new_state) {
+		user_id = n_user_id;
+		device_id = n_device_id;
+		profile_name = null;
+		load_state(new_state);
+	}
+	
+	public JerryUser(String n_user_id, String n_device_id){
+		user_id = n_user_id;
+		device_id = n_device_id;
+		profile_name = null;
+	}
+	public JerryUser(String n_user_id){
+		user_id = n_user_id;
+		device_id = null;
+		profile_name = null;
+	}
 }
--- src/test/java/co/jerri/android/jerry/JerryUserTest.java
+++ src/test/java/co/jerri/android/jerry/JerryUserTest.java
@@ -3,6 +3,7 @@ package co.jerri.android.jerry;
 import junit.framework.TestCase;
 import org.robolectric.RobolectricTestRunner;
 import org.junit.Test;
+import org.junit.Before;
 import org.junit.runner.RunWith;
 import co.jerri.android.jerry.JerryUser;
 
@@ -13,10 +14,24 @@ import static org.junit.Assert.assertThat;
 public class JerryUserTest {
 
     private JerryUser user;
+    private String json_data = "\"profile\": \"free\",\"default\": \"deny\",\"account\": {    \"credits\": 100},\"states\": {    \"take_photo\": [        {\"class_\": \"BinaryRestriction\", \"allow\": True}    ],    \"take_photo_private\": [        {\"class_\": \"AccountAmountRestriction\",                \"account_item\": \"credtis\"}   # typo    ],    \"upload_photo\": [        {\"class_\": \"PerTimeRestriction\", \"limit_to\": 100,                \"duration\": 24 * 60 * 60, \"left\": 3}    ],    \"share_photo\": [        {\"class_\": \"PerTimeRestriction\", \"limit_to\": 100,                \"duration\": 24 * 60 * 60, \"left\": 20},        {\"class_\": \"TotalAmountRestriction\", \"total_max\": 100,                \"left\": 10}    ],    \"share_photo_private\": [        {\"class_\": \"AccountAmountRestriction\",                \"account_item\": \"credits\",                 \"quantity_change\": 35}    ],}}";
+
+    @Before
+    public void setUp() throws Exception {
+    	user = new JerryUser("Mr T.");
+        user.load_state(json_data);
+    }
 
     @Test
-    public void shouldHaveHappySmiles() throws Exception {
-        user = new JerryUser();
-        assertThat(user.user_id, equalTo("error"));
+    public void simpleIDTest() throws Exception {
+        assertThat(user.user_id, equalTo("Mr T."));
+    }
+
+
+    @Test
+    public void simpleDeviceTest() throws Exception {
+    	user = new JerryUser(null, "1234");
+        assertThat(user.user_id, equalTo(null));
+        assertThat(user.device_id, equalTo("1234"));
     }
 }
