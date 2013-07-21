a4b8d6ea4de1bc1120ae0e9e1bb0842ec275b685
Benjamin Kampmann <ben.kampmann@gmail.com>
2013-07-21 19:33:25 +0200

    add first iteration of working parsing

--- src/main/java/co/jerri/android/jerry/JerryUser.java
+++ src/main/java/co/jerri/android/jerry/JerryUser.java
@@ -1,10 +1,24 @@
 package co.jerri.android.jerry;
 
 import java.lang.String;
-import java.util.List;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.lang.Class;
+import java.util.Iterator;
+import org.json.*;
+import java.util.logging.Logger;
 
 class Restriction {
+
+	protected JSONObject settings;
+
+	public Restriction() {
+		settings = null;
+	}
+
+	public Restriction(JSONObject new_settings){
+		settings = new_settings;
+	}
 
 	public boolean allows(String attr, int change){
 		return false;
@@ -22,19 +36,99 @@ class Restriction {
 	}
 }
 
+class BinaryRestriction extends Restriction {
+
+	private boolean allow;
+
+	public BinaryRestriction(JSONObject settings){
+		allow = settings.optBoolean("allow", false);
+	}
+
+	public boolean allows(String attr, int change){
+		return allow;
+	}
+}
+
 public class JerryUser {
 	public String user_id;
 	public String device_id;
 	public String profile_name;
 	public boolean default_reaction;
-	private HashMap<String, List<Restriction>> restrictions;
-	private String profile_state;
+	public HashMap<String, ArrayList<Restriction>> restrictions;
+	private JSONObject profile_state;
 
-	public void load_state(String input){
+	private static Logger LOGGER = Logger.getLogger("JerryUser");
+
+	public void load_state(JSONObject input){
+		if (restrictions == null){
+			restrictions = new HashMap<String, ArrayList<Restriction>>();
+		}
+		profile_name = input.optString("profile_name");
+		profile_state = input;
+		JSONObject states = input.optJSONObject("states");
+		Iterator<String> state_names = states.keys();
+
+		LOGGER.info("loading");
+
+		while(state_names.hasNext()) {
+			String restriction_name = state_names.next();
+			LOGGER.info("Checking: " + restriction_name);
+			JSONArray restrictionsJSON = states.optJSONArray(restriction_name);
+			ArrayList<Restriction> restrictionsList = new ArrayList<Restriction>();
+			LOGGER.info("ListLen: " + restrictionsJSON.length());
+			for (int x=0; x < restrictionsJSON.length(); x++){
+				JSONObject restJSON = restrictionsJSON.optJSONObject(x);
+				if (restJSON == null) { // does not actually ever happen in production
+					LOGGER.warning("Empty objects... hu?");
+					continue;
+				} 
+
+				String className = restJSON.optString("class_");
+				if (className == null) {
+					LOGGER.warning("Object misses class_ definition");
+					continue;
+				}
+				Restriction restObj = null;
+
+				// A map here would be nicer of course
+				if (className.equals("BinaryRestriction")) {
+					LOGGER.info("pling");
+					restObj = new BinaryRestriction(restJSON);
+				}
+
+				if (restObj != null) {
+					restrictionsList.add(restObj);
+				} else {
+					LOGGER.info("Restriction Unknown: " + className);
+				}
+				
+			}
+			LOGGER.info("Restlist: " + restrictionsList.size());
+			if (restrictionsList.size() > 0) {
+				restrictions.put(restriction_name, restrictionsList);
+			}
+		}
 
 	}
 
-	public JerryUser(String n_user_id, String n_device_id, String new_state) {
+	public boolean can(String attr, int change){
+		ArrayList<Restriction> states = restrictions.get(attr);
+		if (states != null) {
+			for(int x=0; x < states.size(); x++){
+				if (states.get(x).allows(attr, change) == false) {
+					return false;
+				}
+			}
+			return true;
+		}
+		return false;
+	}
+
+	public boolean can(String attr) {
+		return can(attr, 1);
+	}
+
+	public JerryUser(String n_user_id, String n_device_id, JSONObject new_state) {
 		user_id = n_user_id;
 		device_id = n_device_id;
 		profile_name = null;
--- src/test/java/co/jerri/android/jerry/JerryUserTest.java
+++ src/test/java/co/jerri/android/jerry/JerryUserTest.java
@@ -6,32 +6,88 @@ import org.junit.Test;
 import org.junit.Before;
 import org.junit.runner.RunWith;
 import co.jerri.android.jerry.JerryUser;
+import org.json.JSONObject;
 
 import static org.hamcrest.CoreMatchers.equalTo;
-import static org.junit.Assert.assertThat;
+import static org.fest.assertions.api.Assertions.*;
 
 @RunWith(RobolectricTestRunner.class)
 public class JerryUserTest {
 
     private JerryUser user;
-    private String json_data = "\"profile\": \"free\",\"default\": \"deny\",\"account\": {    \"credits\": 100},\"states\": {    \"take_photo\": [        {\"class_\": \"BinaryRestriction\", \"allow\": True}    ],    \"take_photo_private\": [        {\"class_\": \"AccountAmountRestriction\",                \"account_item\": \"credtis\"}   # typo    ],    \"upload_photo\": [        {\"class_\": \"PerTimeRestriction\", \"limit_to\": 100,                \"duration\": 24 * 60 * 60, \"left\": 3}    ],    \"share_photo\": [        {\"class_\": \"PerTimeRestriction\", \"limit_to\": 100,                \"duration\": 24 * 60 * 60, \"left\": 20},        {\"class_\": \"TotalAmountRestriction\", \"total_max\": 100,                \"left\": 10}    ],    \"share_photo_private\": [        {\"class_\": \"AccountAmountRestriction\",                \"account_item\": \"credits\",                 \"quantity_change\": 35}    ],}}";
-
-    @Before
-    public void setUp() throws Exception {
-    	user = new JerryUser("Mr T.");
-        user.load_state(json_data);
-    }
+    private String json_data = "{\"profile\": \"free\",\"default\": \"deny\",\"account\": {\"credits\": 100},\"states\": {\"take_photo\": [{\"class_\": \"BinaryRestriction\", \"allow\": True}], \"take_photo_private\": [{\"class_\": \"AccountAmountRestriction\",\"account_item\": \"credtis\"}], \"upload_photo\": [ {\"class_\": \"PerTimeRestriction\", \"limit_to\": 100,\"duration\": 86400, \"left\": 3}],\"share_photo\": [ {\"class_\": \"PerTimeRestriction\", \"limit_to\": 100,\"duration\": 86400, \"left\": 20},{\"class_\": \"TotalAmountRestriction\", \"total_max\": 100,\"left\": 10}],\"share_photo_private\": [{\"class_\": \"AccountAmountRestriction\",\"account_item\": \"credits\", \"quantity_change\": 35}]}}";
 
     @Test
     public void simpleIDTest() throws Exception {
-        assertThat(user.user_id, equalTo("Mr T."));
+		user = new JerryUser("Mr T.");
+        assertThat(user.user_id).isEqualTo("Mr T.");
+        assertThat(user.device_id).isEqualTo(null);
     }
 
 
     @Test
     public void simpleDeviceTest() throws Exception {
     	user = new JerryUser(null, "1234");
-        assertThat(user.user_id, equalTo(null));
-        assertThat(user.device_id, equalTo("1234"));
+        assertThat(user.user_id).isEqualTo(null);
+        assertThat(user.device_id).isEqualTo("1234");
     }
+
+    @Test
+    public void simpleCan() throws Exception {
+    	user = new JerryUser("Mr T.");
+    	JSONObject jsonobj = new JSONObject(json_data);
+    	user.load_state(jsonobj);
+    	assertThat(user.restrictions.size()).isEqualTo(1);
+        assertThat(user.can("take_photo")).isTrue();
+		assertThat(user.can("take_photo", 10)).isTrue();
+
+	}
+	
+    /*@Test
+    public void canExtended() throws Exception {
+    	user = new JerryUser("Mr T.");
+    	JSONObject jsonobj = new JSONObject(json_data);
+    	user.load_state(jsonobj);
+    	assertThat(user.restrictions.size()).isEqualTo(1);
+        assertThat(user.can("take_photo")).isTrue();
+		assertThat(user.can("take_photo", 10)).isTrue();
+
+		assertThat(user.can("upload_photo")).isTrue();
+		assertThat(user.can("upload_photo", 3)).isTrue();
+		assertThat(user.can("upload_photo", 4)).isFalse();
+
+		assertThat(user.can("share_photo")).isTrue();
+		assertThat(user.can("share_photo", 9)).isTrue();
+		// total amount kicks in though daily woud allow it
+		assertThat(user.can("share_photo", 12)).isFalse();
+
+		// do we have credits to share photos privately?
+		assertThat(user.can("share_photo_private")).isTrue();
+		assertThat(user.can("share_photo_private", 2)).isTrue();
+		assertThat(user.can("share_photo_private", 3)).isFalse();
+
+		// typo
+		assertThat(user.can("take_photo_private")).isFalse();
+
+		// do we have credits to share photos privately?
+		user.did("share_photo_private", 1)
+		mocky.calls.should.have.length_of(1)
+		user.can("share_photo_private").should.be.ok
+		user.did("share_photo_private", 2)
+		mocky.calls.should.have.length_of(2)
+		user.can("share_photo_private").should.be.false
+
+		// not defined: default is deny
+		user.can("view_photo").should_not.be.ok
+		user.can("view_photo", 10).should_not.be.ok
+
+		// lets modify and play again:
+		user.did("upload_photo")
+		mocky.calls.should.have.length_of(3)
+		user.can("upload_photo").should.be.ok
+		user.can("upload_photo", 2).should.be.ok
+		user.did("upload_photo", 2)
+		mocky.calls.should.have.length_of(4)
+		user.can("upload_photo").should_not.be.ok
+    }*/
 }
